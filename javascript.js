9edc3d9f3d2cc1a238745c523d7da3868502edcb
PumpkinBaron <mutt-tableware-yam@duck.com>
2026-05-13 14:06:43 -0500

    Rework grid generation logic completely to be more dynamic
    Rather than trying to run a lot of calculations, the functions were
    reworked to make the sizing more accurate and dynamic. The main
    innovation is with generateLine, which, rather than trying to fill
    the container, generates one line at a time. Currently lines are
    repeatedly generated, but the hope is that the could one day be cloned instead, resulting in fewer calculations.
    
    generateLine generates a line of squares. It is used both to fill the boxes and generate them. This is because the same behavior is
    being used in both cases. To distinguish between the needed styles
    in both cases, it accepts a string as an input that is applied to
    each item in the line. The other inputs are the container to append (for instance, the to level container in the case of the boxes), the number of squares in a line,
    
    This means the basic grid generation logic is:
    (0) generateGrid() clears the top level container. ->
    (1) Input the boxes of squares per line into generateGrid()->
    (2) generateGrid() selects a box width that will fit inside the top
    level container and be filled an even number of tmes ->
    (3) Generate grid uses generate line to make a line of boxes, then
    repeats the same number of times as the number of boxes in a line.
    (4) Having generated the grid, generateGrid then fills the boxes.
    (5) GenerateGrid add the button back in.
    
    For some reason certain sizes have a white margin like area. The
    100x100 grid just looks white, but you can still change its color.
    This may be a result of me changing more than one pixel's color at
    a time, which was not in the original instructions and added a lot
    of complexity. I'm giving up and leaving it for now since I feel
    this is still more satisfying than changing one pixel at a time.
    
    There is also some console spam that will need to be removed. Since
    I don't quite understand how to generate tests yet, I'm relying on
    this to understand what it's doing, but it uses up too many system
    resources for the completed version.

--- javascript.js
+++ javascript.js
@@ -1,54 +1,126 @@
-const container = document.querySelector(".container");
-const button = document.createElement("button")
+/* Variables */
 
-let color = "black";
-let numberOfSquares = 16;
+const container = document.querySelector(".container");
+const containerWidth = container.offsetWidth
+const button = document.createElement("button");
+button.textContent = "Click to resize grid.";
+
+let color = "black"
+
+/* Functions */
 
 function changeColor(div) {
     div.style.backgroundColor = color;
 }
 
-function fillWithDivs(div) {
-    for (i = 0; i < 100; i++){
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("filling");
-        div.appendChild(newDiv);
+function findFillSize(containerInput){
+    console.log("Running find fill size...")
+    const borderlessWidth = containerInput.clientWidth - 2;
+    console.log(`containerInput.offsetWidth = ${borderlessWidth}`);
+    let fillingWidth = "";
+    let numberOfFillings = "";
 
+    if (!(borderlessWidth % 3 === 0 ||
+         borderlessWidth % 4 === 0 ||
+        borderlessWidth % 5 === 0)) {
+           return console.error(`Attempted width ${borderlessWidth} is not evenly
+           divisible by 3, 4, or 5.`);   
+    } if (borderlessWidth % 5 === 0) {
+        fillingWidth = 5;
+    } if (borderlessWidth % 4 === 0) {
+        fillingWidth = 4;
+    } if (borderlessWidth % 3 === 0) {
+        fillingWidth = 3;
     }
+    numberOfFillings = borderlessWidth / fillingWidth;
+    console.log(`Find fill size returned filling width "${fillingWidth}" and 
+        number of fillings "${numberOfFillings}"`);
+    return [fillingWidth, numberOfFillings];
 }
 
-function createDiv() {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("box");
-    fillWithDivs(newDiv);
-    container.appendChild(newDiv);
-
+function generateLine(
+    containerToFill, squareWidth, squaresPerLine, squareClass) {
+   console.log(`Running generate line with inputs
+            containerToFill "${containerToFill}", square width "${squareWidth}", squares per line 
+            "${squaresPerLine}", and class "${squareClass}".`)
+        const lineOfSquares = document.createElement("div");
+        for (let i = 0; i < squaresPerLine; i++ ) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add(squareClass);
+            newSquare.style.width = squareWidth + "px";
+            newSquare.style.height = squareWidth + "px";  
+            lineOfSquares.appendChild(newSquare);
+        }
+        containerToFill.appendChild(lineOfSquares);
 }
 
-function createGrid(squaresPerLine){
-for (let i=0; i < squaresPerLine ** 2; i++) {
-    createDiv();
-     }
+function fillWithGreySquares(containerToFill) {
+        console.log("Filling container with grey squares...")
+        const sizeInfo = findFillSize(containerToFill);
+        const squareWidth = sizeInfo[0];
+        const squaresPerLine = sizeInfo[1];
+        console.log(`Running generate line via fillWithGreySquares with inputs
+            container "${container}", square width "${squareWidth}", squares per line 
+            "${squaresPerLine}", and class "filling".`)
+
+        for (let i = 0; i < squaresPerLine; i++ ) {
+            generateLine(containerToFill, squareWidth, squaresPerLine, "filling");
+        }
+}
+
+function findAcceptableBoxWidth(squaresPerLine) {
+        console.log("Finding acceptable box width...")
+    let borderlessWidth = Math.floor(containerWidth / squaresPerLine - 2);
+    while (!(borderlessWidth % 3 === 0 || 
+            borderlessWidth % 4 === 0 || 
+            borderlessWidth % 5 === 0)) {
+                console.log(`Current border with is ${borderlessWidth}`);
+                borderlessWidth--;
+                if (borderlessWidth < 1) console.error("borderless width went below 1");
+            }
+            return borderlessWidth + 2;
+}
+
+function generateGrid(squaresPerLine) {
+        console.log("Generating the grid...")
+    const boxWidth = findAcceptableBoxWidth(squaresPerLine);
+        console.log(`generateGrid found acceptable box width ${boxWidth}`)
+    /* Clear top level container */
+    container.textContent = "";
+    /* Make a grid */
+    for (let i = 0; i < squaresPerLine; i++ ) {
+        console.log(`Running generate line via generateGrid with inputs
+            container "${container}", box width "${boxWidth}", squares per line 
+            "${squaresPerLine}", and class "box".`)
+        generateLine(container, boxWidth, squaresPerLine, "box");
+    }
+    /* Fill the squares */
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(fillWithGreySquares);
+
+    /* Add a button */
+    newPara = document.createElement("p");
+    container.appendChild(newPara);
+    newPara.appendChild(button);
+}
+
+/* Event Listeners */
+
 container.addEventListener("mouseover", event => {
     const targetDiv = event.target;
     if (targetDiv.className === "filling") {
     console.log('Hover activated...');
     changeColor(targetDiv); 
     }
-})
-}
+});
 
-createGrid(numberOfSquares);
-
-
-
-button.textContent = "Click to resize grid.";
 button.addEventListener("click", () => {
     do {    
-    numberOfSquares = Number(window.prompt("How many squares per line", ""));
-    console.log(`Number of squares changed to ${numberOfSquares}`)
-    } while (!Number.isInteger(numberOfSquares));
-/*     clearSquares(); */
+        numberOfSquares = Number(window.prompt(`How many squares per line? 
+            Input a number between 1 and 100`, ""));
+        console.log(`Number of squares changed to ${numberOfSquares}`)
+        } while (!Number.isInteger(numberOfSquares))
+        generateGrid(numberOfSquares);
+});
 
-})
-container.appendChild(button);
+generateGrid(16);
